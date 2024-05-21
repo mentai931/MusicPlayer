e6ed2059c175721888683eab091f79d7f7c41fd2
Mentai <mert.atalay@outlook.de>
2024-05-21 14:02:38 +0200

    Initial Commit

---
+++ MusicPlayer/main.cpp
@@ -0,0 +1,71 @@
+#include "SDL.h"
+#include "SDL2_mixer/include/SDL_mixer.h"
+#include <iostream>
+static const char* MY_COOL_MP3 = "Rain.mp3";
+
+int main(int argc, char** argv) {  
+    SDL_Window *window = nullptr;
+    SDL_Renderer* renderer = nullptr;
+    SDL_Event event;
+    
+    bool isRunning = true;
+    int result = 0;
+    int flags = MIX_INIT_MP3;
+
+    if (SDL_Init(SDL_INIT_AUDIO /*| SDL_INIT_VIDEO ^ */ ) < 0) {
+        printf("Failed to init SDL\n");
+        exit(1);
+    }
+
+    
+    if (flags != (result = Mix_Init(flags))) {
+        printf("Could not initialize mixer (result: %d).\n", result);
+        printf("Mix_Init: %s\n", Mix_GetError());
+        exit(1);
+    }
+
+
+    Mix_OpenAudio(22050, AUDIO_S16SYS, 2, 640);
+    Mix_Music* music = Mix_LoadMUS(MY_COOL_MP3);
+    //Mix_PlayMusic(music, 1);
+                   
+    SDL_CreateWindowAndRenderer(1280, 720, 0, &window, &renderer);
+    while (1) {
+     
+
+            while (SDL_PollEvent(&event)) {
+
+                if (event.type == SDL_KEYDOWN)
+                    std::cout << "Key press\n";
+
+                switch (event.type) {
+                case SDL_KEYDOWN:
+                    std::cout << "Key press detected amk \n";
+                    break;
+
+                case SDL_KEYUP:
+                    std::cout << "Key press not detected \n";
+
+                    break;
+
+                default:
+                    break;
+                }
+            }
+            SDL_Delay(1000);
+     }
+        
+
+
+
+    /*while (!SDL_QuitRequested()) {
+        SDL_Delay(250);
+
+    }*/
+    
+
+
+    Mix_FreeMusic(music);
+    SDL_Quit();
+    return 0;
+}
